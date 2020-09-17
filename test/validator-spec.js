b4b1036c0800d7cd234051c0f3f762a4ca99abdb
Peter Fagerkvist <p.fagerkvist@gmail.com>
2020-09-17 21:07:42 +0200

    use module for test

---
+++ test/validator-spec.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+const validator = require("../lib/validator");
+
+validator = require('../lib/validator');
+
+describe("A Validator", function () {
+  it("will return error.nonpositive for not strictly positive numbers", function () {
+    assert.deepStrictEqual(validator(0), ["error.nonpositive"]);
+  });
+});
