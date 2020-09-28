9a8c82e4aaac8a747aae6d14ec8ff11738da68d5
Peter Fagerkvist <p.fagerkvist@gmail.com>
2020-09-28 11:26:11 +0200

    Split test too separate modules

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,9 +1,39 @@
-var assert = require("assert");
-const validator = require("../lib/validator");
-
+const chai = require("chai"),
+  expect = chai.expect,
+  validator = require("../lib/validator");
 
 describe("A Validator", function () {
-  it("will return error.nonpositive for not strictly positive numbers", function () {
-    assert.deepStrictEqual(validator(0), ["error.nonpositive"]);
+  it("will return no errors for valid numbers", function () {
+    expect(validator(7)).to.be.empty;
+  });
+
+});
+
+describe("will include error.nonpositive for not strictly positive numers:", function () {
+  it("like 0", function () {
+    expect(validator(0)).to.include("error.nonpositive");
+  });
+
+  it("like -2", function () {
+    expect(validator(-2)).to.include("error.nonpositive");
+  });
+});
+
+describe("will include error.three for divisible by 3 numbers:", function () {
+  it("like 3", function () {
+    expect(validator(3)).to.include("error.three");
+  });
+  it("like 15", function () {
+    expect(validator(15)).to.include("error.three");
+  });
+});
+
+describe("will return error.five for divisible by 5 numbers:", function () {
+  it("like 5", function () {
+    expect(validator(5)).to.include("error.five");
+  });
+
+  it("like 15", function () {
+    expect(validator(15)).to.include("error.five");
   });
 });
